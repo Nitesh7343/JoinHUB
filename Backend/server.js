12cd6c342054ba8f315a4aea37f8ea5c41eabfad
Nitesh7343 <niteshsingh7343@gmail.com>
2026-05-24 23:59:09 +0530

    Testing Server

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,10 @@ app.get("/",(req,res)=>{
     res.send("Home Page");
 })
 
+app.get("/sessions",(req,res)=>{
+    res.send("Session page");
+})
+
 app.listen(port, () => {
     console.log(`Server is running on ${PORT}`);
 })
