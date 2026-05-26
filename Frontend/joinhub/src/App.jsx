8094958a1472f141a6284a2f01c775418d454e74
Nitesh7343 <niteshsingh7343@gmail.com>
2026-05-26 23:10:40 +0530

    Updated Router and added different pages

--- Frontend/joinhub/src/App.jsx
+++ Frontend/joinhub/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Navbar from './components/Navbar.jsx'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx'
+import {Routes, Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className=''>
-      <Navbar/>
-    </div>
+    <>
+    <Navbar/>
+
+    <Routes>
+      <Route path = "/" element = {<Home/>}/>
+      <Route path = "/Login" element = {<Login/>}/>
+      <Route path = "/Signup" element = {<Signup/>}/>
+    </Routes>
+    </>
   )
 }
 
