975bd2986c3b1f5b9d496c1032d11fecd54611aa
Nitesh7343 <niteshsingh7343@gmail.com>
2026-05-28 23:25:21 +0530

    Home and Sessions route addded

--- Frontend/joinhub/src/App.jsx
+++ Frontend/joinhub/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar.jsx'
 import Home from './pages/Home.jsx'
 import Login from './pages/Login.jsx'
 import Signup from './pages/Signup.jsx'
+import Sessions from './pages/Sessions.jsx'
 import {Routes, Route} from 'react-router-dom'
 
 const App = () => {
@@ -12,8 +13,9 @@ const App = () => {
 
     <Routes>
       <Route path = "/" element = {<Home/>}/>
-      <Route path = "/Login" element = {<Login/>}/>
-      <Route path = "/Signup" element = {<Signup/>}/>
+      <Route path = "/login" element = {<Login/>}/>
+      <Route path = "/signup" element = {<Signup/>}/>
+      <Route path = '/sessions' element = {<Sessions/>}/>
     </Routes>
     </>
   )
