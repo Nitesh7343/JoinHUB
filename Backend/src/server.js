27e8a711181841a49116523e20e540ab2d28c8e4
devesh-chaudhary-2004 <chaudharydebu008@gmail.com>
2026-05-28 14:40:44 +0530

    basic backend structure defined

---
+++ Backend/src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("JoinHub API Running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
