f0a561c5e98fdfa74aa9833c92b8fe283be316f1
Nitesh7343 <niteshsingh7343@gmail.com>
2026-05-30 15:54:39 +0530

    LINK FOR organize session updated

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logoH.png";
+import Searchbar from "./Searchbar.jsx"
 
 const Navbar = () => {
   const isLoggedIn = false;
@@ -15,34 +16,7 @@ const Navbar = () => {
       </div>
 
       {/* CENTER SECTION */}
-      <div className="w-[40%] mb-3">
-        <div className="flex items-center border border-gray-300 rounded-full overflow-hidden shadow-sm bg-white transition duration-300 hover:bg-gray-100  focus-within:ring-1 focus-within:ring-green-600">
-          {/* INPUT */}
-          <input
-            type="text"
-            placeholder="Search for sessions..."
-            className="w-full px-5 py-2 outline-none bg-transparent"
-          />
-
-          {/* BUTTON */}
-          <button className="text-green-600 px-5 py-2 hover:bg-green-600 hover:text-white transition">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-5 h-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 21l-5.2-5.2m0 0A7.5 7.5 0 105.8 5.8a7.5 7.5 0 0010.6 10.6z"
-              />
-            </svg>
-          </button>
-        </div>
-      </div>
+      <Searchbar/>
 
       {/* RIGHT SECTION */}
       <div className="flex items-center gap-6">
@@ -66,7 +40,7 @@ const Navbar = () => {
               </Link>
 
               <Link
-                to="/sessions/create"
+                to="/create-session"
                 className="block px-5 py-3 hover:bg-gray-100 transition"
               >
                 Organize Session
