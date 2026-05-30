e830ac5a2436119570c7c1225ccb70d3fdfed72b
devesh-chaudhary-2004 <chaudharydebu008@gmail.com>
2026-05-30 09:31:04 +0530

    completed authentication part, added middleware and hashed password

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import admin from "./config/firebase.js";
+import authRoutes from "./routes/authRoutes.js";
+
 
 dotenv.config();
 connectDB();
@@ -21,6 +23,9 @@ app.get("/", (req, res) => {
 app.get("/test", (req, res) => {
   res.send("Firebase Admin Connected");
 });
+
+app.use("/auth", authRoutes);
+app.use("/login", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
