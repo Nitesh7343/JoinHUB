c853320f420a60e24d5b8321906730e6e58288be
Nitesh7343 <niteshsingh7343@gmail.com>
2026-06-08 22:52:12 +0530

    sesssion model created

---
+++ Backend/src/models/session.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const sessionSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    category: {
+      type: String,
+      required: true,
+    },
+
+    date: {
+      type: Date,
+      required: true,
+    },
+
+    language: {
+      type: String,
+      default: "English",
+    },
+
+    isPaid: {
+      type: Boolean,
+      default: false,
+    },
+
+    price: {
+      type: Number,
+      default: 0,
+    },
+
+    host: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Session", sessionSchema);
