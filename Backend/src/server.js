965c01b5b6f81a5131fff31b1919c08a8ae7e140
Nitesh7343 <niteshsingh7343@gmail.com>
2026-06-09 23:58:27 +0530

    server js tested

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
-import admin from "./config/firebase.js";
+//import admin from "./config/firebase.js";
 import authRoutes from "./routes/authRoutes.js";
 import sessionRoutes from './routes/sessionRoutes.js'
 
