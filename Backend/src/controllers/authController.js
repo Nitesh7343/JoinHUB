545efa8435c93b14d07d9ca69e6c155c98becec2
Nitesh7343 <niteshsingh7343@gmail.com>
2026-06-15 23:47:38 +0530

    Updated authController

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -6,7 +6,7 @@ export const register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
-    const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({  });
 
     if (existingUser) {
       return res.status(400).json({
