cfdacefdec71f157b7c22b4ec5c56a57edaca4dc
Nitesh7343 <niteshsingh7343@gmail.com>
2026-06-15 23:49:41 +0530

    Updated user model

--- Backend/src/models/user.js
+++ Backend/src/models/user.js
@@ -17,6 +17,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    sessions: {
+      info:{
+        
+      }
+    }
   },
   {
     timestamps: true,
